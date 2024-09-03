083db6ce9fa2c0ca011a0b9ff2a318af2562a43e
gooniusmaximus <spham108@student.hbuhsd.edu>
2024-09-03 10:47:09 -0700

    "always be gooning"

--- problem2.java
+++ problem2.java
@@ -1,1 +1,9 @@
-
+public class problem2
+{
+  public static void main(String[] args)
+  {
+    System.out.print("Coding is fun!");
+        System.out.print("Coding is fun!");
+            System.out.print("Coding is fun!");
+  }
+}
--- problem3.java
+++ problem3.java
@@ -1,1 +1,10 @@
-
+public class problem2
+{
+  public static void main(String[] args)
+  {
+    System.out.print("=========");
+    System.out.print("= 0   0 =    Java is Awesome!");
+    System.out.print("=   v   =");
+    System.out.print("=========");
+  }
+}
